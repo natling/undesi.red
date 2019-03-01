efeb785887c89e25bc2f5f4f1dfd5be6384bd089
Natalie Braginsky <hi@natalie.computer>
2019-02-28 22:06:58 -0500

    update shuffleArray

--- js/undesired.js
+++ js/undesired.js
@@ -187,11 +187,8 @@ const f = {
 	shuffleArray : array => {
 		for (let i = array.length - 1; i > 0; i--) {
 			const j = Math.floor(Math.random() * (i + 1));
-			const temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
+			[array[i], array[j]] = [array[j], array[i]];
 		}
-
 		return array;
 	},
 };
